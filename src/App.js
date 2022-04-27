25812755813992f7254b803362d116a98dc974ab
Jasper Katzban <jasperkatzban@gmail.com>
2022-04-27 00:42:50 -0400

    move navbar to component

--- src/App.js
+++ src/App.js
@@ -1,67 +1,17 @@
 import React, { useState } from 'react';
 
 import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import { Nav, Row, Col} from 'react-bootstrap';
+
+import MainNavbar from './components/MainNavbar/MainNavbar';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-// import LogoMonoColor from './images/logo-mono-color.svg';
 
 function App() {
   return (
     <Container fluid className='MainBackground'>
       <Container className='MainContent'> 
-      <Container>
-        <Navbar bg="none" variant="light">
-          <Container className='justify-content-between'>
-            <Col xs={3}>
-              <Navbar.Brand href="#home">
-                <img
-                  src='./images/logo-mono-color.svg'
-                  className="d-inline-block align-top"
-                  alt="Olin Hydro logo"
-                />
-              </Navbar.Brand>
-            </Col>
-            <Col xs="auto">
-              <Container className='navMenu'>
-                <Nav className="me-auto justify-content-center">
-                  <Nav.Link href="#home">Home</Nav.Link>
-                  <Nav.Link href="#logs">Logs</Nav.Link>
-                  <Nav.Link href="#config">Config</Nav.Link>
-                  <Nav.Link href="#camera">Camera</Nav.Link>
-                </Nav>
-              </Container>
-            </Col>
-            <Col xs={3}>
-                <Row className='justify-content-between'>
-                  <Col>
-                    <img
-                      src='./images/navbar/help.svg'
-                      className="d-inline-block align-center"
-                      alt="help"
-                    />
-                  </Col>
-                  <Col>
-                    <img
-                      src='./images/navbar/github.svg'
-                      className="d-inline-block align-center"
-                      alt="view on github"
-                    />
-                  </Col>
-                  <Col>
-                    <img
-                      src='./images/navbar/settings.svg'
-                      className="d-inline-block align-center"
-                      alt="settings"
-                    />
-                  </Col>
-                </Row>
-            </Col>
-          </Container>
-        </Navbar>
-        </Container>
+      <MainNavbar></MainNavbar>
       </Container>
     </Container>
   );
