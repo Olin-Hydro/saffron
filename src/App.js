4cbf4ac1095188fe77d5767a104fb0fc00614a35
Jasper Katzban <jasperkatzban@gmail.com>
2022-04-26 23:13:36 -0400

    add main background

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import Home from "./components/Home";
+import React, { useState } from 'react';
+
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Home></Home>
+    <div className="MainBackground">
+      
     </div>
   );
 }
