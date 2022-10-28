0b3433722b7c77a0d1c0a95c57915841a3330195
Jasper Katzban <jasperkatzban@gmail.com>
2022-10-28 16:54:14 -0400

    mui formatting of top level App view

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React, { useState } from 'react';
 
-// import Container from 'react-bootstrap/Container';
 import { Container } from '@mui/system';
 
 import MainNavbar from './components/MainNavbar/MainNavbar';
 import Home from './pages/Home/Home';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
 
 function App() {
   return (
-    // <Container fluid className='MainBackground'>
-    //   <Container className='MainContent'> 
-    //     
-    //   </Container>
-    // </Container>
     <Container>
       <MainNavbar></MainNavbar>
       <Home></Home>
