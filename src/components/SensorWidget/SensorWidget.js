d1e0f469eff09a2dc4ecad89823c656da28ca3a5
Jasper Katzban <jasperkatzban@gmail.com>
2022-10-28 16:52:08 -0400

    mui formatting of SensorWidget

--- src/components/SensorWidget/SensorWidget.js
+++ src/components/SensorWidget/SensorWidget.js
@@ -1,62 +1,123 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Grid, Stack, Typography, IconButton, SvgIcon } from "@mui/material";
+import { Grid, Typography, IconButton, SvgIcon } from "@mui/material";
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 import { ReactComponent as ECIcon } from "../../icons/ec.svg";
-  
+
 import SensorGraph from "./SensorGraph"
 
-const SensorWidget = ({ sensorType, sensorData, mean, min, max, sensorState }) => {  
+const SensorWidget = ({ sensorType, sensorData, mean, min, max, sensorState }) => {
 
   return (
-    <Grid item xs="auto">
-      <Stack spacing={7} xs="auto">
-        {/* title */}
+    // Top level grid item is necessary here because this item fits into a grid
+    <Grid
+      container
+      item
+      direction="column"
+      justifyContent="space-between"
+      alignItems="stretch"
+      xs="auto">
+      {/* title */}
+      <Grid
+        container
+        justifyContent="space-between"
+        alignItems="center"
+        spacing={5}
+      >
         <Grid
-          container
-          justifyContent="space-between"
-          alignItems="center"
-          spacing={5}
-        >
-          <Grid item xs="auto">
-            {/* TODO: add logic for icon selection here */}
-            <SvgIcon component={ECIcon} inheritViewBox />
+          item
+          xs="auto">
+          {/* TODO: add logic for icon selection here */}
+          <SvgIcon
+            component={ECIcon}
+            inheritViewBox
+            fontSize="small"
+          />
+        </Grid>
+        <Grid
+          item
+          xs>
+          <Typography
+            variant="widgetTitle"
+            color="text.primary">Title</Typography>
+        </Grid>
+        <Grid
+          item
+          xs="auto">
+          <IconButton aria-label="settings">
+            <OpenInNewIcon
+              color="light"
+              fontSize="small"
+            ></OpenInNewIcon>
+          </IconButton>
+        </Grid>
+      </Grid>
+      {/* Graph */}
+      <SensorGraph sensorData={sensorData}></SensorGraph>
+      {/* Numerical stats area */}
+      <Grid
+        container
+        item
+        direction="row"
+        justifyContent="space-evenly"
+        alignItems="flex-end"
+        spacing={5}>
+        {/* Avg stat */}
+        <Grid
+          item
+          direction="column"
+          justifyContent="flex-end"
+          alignItems="center">
+          <Grid
+            item
+            align="center"
+            marginBottom={-8}>
+            <Typography
+              variant="widgetStat"
+              color="text.light">
+              ###
+            </Typography>
           </Grid>
-          <Grid item xs>
-            <Typography variant="widgetTitle" color="text.primary">Title</Typography>
+          <Grid
+            item
+            align="center">
+            <Typography
+              variant="widgetStatLabel"
+              color="text.light">mean</Typography>
           </Grid>
-          <Grid item xs="auto">
-            <IconButton aria-label="settings">
-              <OpenInNewIcon color="light"></OpenInNewIcon>
-            </IconButton>
+          {/* </Grid> */}
+        </Grid>
+        {/* hi / lo stats */}
+        <Grid
+          item
+          direction="column"
+          justifyContent="flex-end"
+          alignItems="center">
+          <Grid
+            item
+            align="center">
+            <Typography
+              variant="widgetStatLabel"
+              color="text.light">hi </Typography>
+            <Typography
+              variant="widgetStatSmall"
+              color="text.light">###</Typography>
+          </Grid>
+          <Grid
+            item
+            align="center">
+            <Typography
+              variant="widgetStatLabel"
+              color="text.light">lo </Typography>
+            {/* To-do update the widgetStatSmall to have same text baseline as label */}
+            <Typography
+              variant="widgetStatSmall"
+              color="text.light"
+            >###</Typography>
           </Grid>
         </Grid>
-        {/* Graph */}
-          <SensorGraph sensorData={sensorData}></SensorGraph>
-        {/* Numerical stats */}
-        <Grid container spacing={5}>
-          {/* Avg stat */}
-          <Grid item direction="column">
-            <Grid item>
-              <Typography variant="widgetStat" color="text.light">###</Typography>
-            </Grid>
-            <Grid item>
-              <Typography variant="widgetStatLabel" color="text.light">mean</Typography>
-            </Grid>
-          </Grid>
-          {/* hi / lo stats */}
-          <Grid item direction="column">
-            <Grid item >
-              <Typography variant="widgetStatLabel" color="text.light">hi </Typography>
-              <Typography variant="widgetStatSmall" color="text.light">###</Typography>
-            </Grid>
-            <Grid item>
-              <Typography variant="widgetStatLabel" color="text.light">lo </Typography>
-              <Typography variant="widgetStatSmall" color="text.light">###</Typography>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Stack>
+        {/* </Grid> */}
+      </Grid>
     </Grid>
   );
 };
