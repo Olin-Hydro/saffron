35f61716896eba11cfc8b6fc7ee47735c011a25c
Jasper Katzban <jasperkatzban@gmail.com>
2022-11-08 20:17:43 -0500

    Standardize CardContent bottom padding

--- src/components/WidgetWrapper.tsx
+++ src/components/WidgetWrapper.tsx
@@ -9,13 +9,16 @@ import { Card, CardContent, Grid } from "@mui/material";
 const WidgetWrapper = ({ children }) => {  
 
   return (
-    <Card >
-      <CardContent>
+    <Card>
+      {/* Todo: override CardContent bottom padding in theme.ts */}
+      <CardContent sx={{ '&:last-child': { pb: 8 }}}>
+      {/* <CardContent> */}
           <Grid 
             container
             direction="row"
             justifyContent="space-between"
-            spacing={13}>
+            spacing={13}
+            >
             {children}
           </Grid>
       </CardContent>
