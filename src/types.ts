cffbe79bff294af824809942681ed9fc63e2a875
Jasper Katzban <jasperkatzban@gmail.com>
2023-03-20 21:36:53 -0400

    update interfaces for api responses

--- src/types.ts
+++ src/types.ts
@@ -1,73 +1,126 @@
 
-interface Sensor {
-    id: string;
-    name: string;
+interface Command {
+    _id: string;
+    cmd: number;
+    type: string;
     garden_id: string;
-    created_at:string;
+    created_at: string;
+    updated_at: string;
 }
 
-interface SchActuator {
-    id: string;
+interface Config {
+    _id: string;
     name: string;
-    garden_id: string;
-    created_at:string;
+    sensor_schedule: SensorSchedule[];
+    ra_actuators: RASchedule[];
+    sa_schedule: SASchedule[];
+    created_at: string;
+    updated_at: string;
 }
 
-interface ReactiveActuator {
-    id: string;
+interface ConfigUpdate {
     name: string;
-    sensor_id: string;
-    created_at:string;
+    sensor_schedule: SensorSchedule[];
+    ra_actuators: RASchedule[];
+    sa_schedule: SASchedule[];
 }
 
 interface Garden {
-    id: string;
-    name: string; 
-    config: OpConfig; 
-    location: string; 
+    _id: string;
+    name: string;
+    location: string;
+    config_id: string;
     created_at: string;
+    updated_at: string;
 }
 
-
-interface LogReading {
-    id: string;
-    sensor_id: string;
-    value: number;
-    created_at: string;
+interface GardenUpdate {
+    name: string;
+    location: string;
+    config_id: string;
 }
 
-interface Action {
-    id: string;
-    actuator_id: string;
-    data: string;
-    created_at:string;
-}
-
-interface OpConfig {
-    id: string;
-    name: string; 
-    sensors: SSchedule[];
-    scheduled_actuators: SASchedule[];
-    reactive_actuators: RASchedule[];
-    created_at: string;
-}
-
-interface SSchedule {
-    S_id: string;
-    interval: number;
-}
-
-interface SASchedule {
-    SA_id: string;
-    on: any[];
-    off: any[];
+interface HTTPValidationError {
+    detail: ValidationError[]
 }
 
 interface RASchedule {
-    RA_id: string;
+    ra_id: string;
     interval: number;
     threshold: number;
     duration: number;
     threshold_type: number;
 }
 
+interface RAUpdate {
+    name: string
+}
+
+interface ReactiveAction {
+    _id: string;
+    actuator_id: string;
+    data: string;
+    created_at: string;
+    updated_at: string;
+}
+
+interface ReactiveActuator {
+    _id: string;
+    name: string;
+    sensor_id: string;
+    created_at: string;
+    updated_at: string;
+}
+
+interface Reading {
+    _id: string;
+    sensor_id: string;
+    value: number;
+    created_at: string;
+    updated_at: string;
+}
+
+interface SASchedule {
+    sa_id: string;
+    on: string[];
+    off: string[];
+}
+
+interface SAUpdate {
+    name: string;
+}
+
+interface ScheduledAction {
+    _id: string;
+    actuator_id: string;
+    data: string;
+    created_at: string;
+    updated_at: string;
+}
+
+interface ScheduledActuator {
+    _id: string;
+    name: string;
+    garden_id: string;
+    created_at: string;
+    updated_at: string;
+}
+
+interface Sensor {
+    _id: string;
+    name: string;
+    garden_id: string;
+    created_at: string;
+    updated_at: string;
+}
+
+interface SensorSchedule {
+    sensor_id: string;
+    interval: number
+}
+
+interface ValidationError {
+    loc: string[];
+    msg: string;
+    type: string;
+}
