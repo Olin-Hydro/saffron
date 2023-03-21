2d2974a9d0b06b126a04f6f808f6ec35f70511c9
Jasper Katzban <jasperkatzban@gmail.com>
2023-03-20 21:25:56 -0400

    add api key and frontend host url to config

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
-// const REACT_APP_API_URL = "http://localhost:8000/";
-const REACT_APP_API_URL = "http://hydroapi-env.eba-p7wttzu3.us-east-1.elasticbeanstalk.com/";
-export { REACT_APP_API_URL };
+const REACT_APP_URL = "http://localhost:3000" // change this to wherever frontend is hosted
+const REACT_APP_API_URL = "http://localhost:8000/"; // change this to wherever API is hosted
+const REACT_APP_API_KEY = ""; // you'll need one of these!
+export { REACT_APP_URL, REACT_APP_API_URL, REACT_APP_API_KEY };
